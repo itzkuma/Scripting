32b7cab23c1b497ba48051c953370a23b5748f2e
itzkuma <60122679+itzkuma@users.noreply.github.com>
2020-02-10 15:46:15 -0500

    Add files via upload

---
+++ malditoproblema.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximominimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("ingrese el numero de datos (n):");
+
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0, min = 200;
+            string nombremax = ("");
+            string nombremin = ("");
+
+            while (i < n)
+            {
+                Console.Write("nombre:");
+                string nombre = (Console.ReadLine());
+                Console.Write("edad:");
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad > max)
+                {
+                    max = edad;
+                    nombremax = nombre;
+                
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    nombremin = nombre;
+                }
+
+                total += edad;
+
+                i++;
+            }
+            double promedio = total / n;
+            Console.WriteLine("promedio=" + promedio);
+            Console.WriteLine(nombremax  + max);
+            Console.WriteLine(nombremin + min);
+        }
+    }
+}
